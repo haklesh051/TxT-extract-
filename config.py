f48627fcf354dceaba0ce5f05146701fe1d87037
haklesh051 <hakleshbairwa051@gmail.com>
2025-06-08 22:14:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config:
+    # Telegram API details from https://my.telegram.org
+    API_ID = 23977225
+    API_HASH = "969c399e6aea5032314a2e17cab428be"
+    
+    # Bot token from BotFather
+    BOT_TOKEN = "7654456160:AAGT2kmP5Q79Ki5krjje3kgC6wYU-QKEz-A"
+    
+    # Only allow this user to use the bot
+    AUTH_USERS = [7681589139]
